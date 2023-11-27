05609aca1f593d7f1dc61200bf3d096369cc4331
abulisu12 <abulfazlsoultnii12@gmail.com>
2023-11-27 13:03:42 +0330

    Complete Html & Css
    - Finish bone (style) of calculator with Html & Css

--- src/compnents/CCalculator.jsx
+++ src/compnents/CCalculator.jsx
@@ -1,7 +1,6 @@
 import styles from "./CCalculator.module.css";
 import { CInputBox } from "./CInputBox";
 import { CCalculatorBody } from "./CCalculatorBody";
-import { useState } from "react";
 export function CCalculator() {
   return (
     <>
